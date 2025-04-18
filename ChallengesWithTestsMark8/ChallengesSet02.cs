435a8f3030a2243b84277e6d6505d0e224540821
Dlove <dlove327@gmail.com>
2025-04-17 20:03:01 -0500

    Weekly2Complete

--- ChallengesWithTestsMark8/ChallengesSet02.cs
+++ ChallengesWithTestsMark8/ChallengesSet02.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace ChallengesWithTestsMark8
 {
@@ -7,52 +8,123 @@ namespace ChallengesWithTestsMark8
     {
         public bool CharacterIsALetter(char c)
         {
-            throw new NotImplementedException();
+            return char.IsLetter(c);
         }
 
         public bool CountOfElementsIsEven(string[] vals)
         {
-            throw new NotImplementedException();
+            if (vals.Length % 2 == 0)
+            {
+                return true;
+            }
+            return false;
         }
 
-        public bool IsNumberEven(int number)
+        public bool IsNumberEven(int number) //They're giving us an int number, and returning a true or false. I want to know if it's even.
         {
-            throw new NotImplementedException();
+            if (number % 2 == 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
-        public bool IsNumberOdd(int num)
+        public bool IsNumberOdd(int num) //Got it on the first try!
         {
-            throw new NotImplementedException();
+            if (num % 2 != 0)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
         }
 
         public double SumOfMinAndMax(IEnumerable<double> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return 0;
+            }
+
+            if (numbers.Count() == 0)
+            {
+                return 0;
+            }
+            return numbers.Min() + numbers.Max();
         }
 
-        public int GetLengthOfShortestString(string str1, string str2)
+        public int GetLengthOfShortestString(string str1, string str2) //They're giving me two strings, and I'm returning  an integer.
         {
-            throw new NotImplementedException();
+            if (str1.Length < str2.Length)
+            {
+                return str1.Length;
+            }
+            else
+            {
+                return str2.Length;
+            }
         }
 
         public int Sum(int[] numbers)
         {
-            throw new NotImplementedException();
+            //Linq
+            if (numbers == null) //I need to loop through these numbers and add them up as I go.
+            {
+                return 0;
+            }
+            int sum = 0;
+
+            foreach (var num in numbers)
+            {
+                sum += num; //sum = sum + num
+            }
+
+            return sum; 
         }
 
         public int SumEvens(int[] numbers)
         {
-            throw new NotImplementedException();
+           if (numbers == null)
+           {
+                return 0;
+           }
+           
+           var evenSum = 0;
+
+           for (int i = 0; i < numbers.Length; i++)
+           {
+               if (numbers[i] % 2 == 0)
+               {
+                   evenSum += numbers[i];
+               }
+           }
+           return evenSum;
         }
 
         public bool IsSumOdd(List<int> numbers)
         {
-            throw new NotImplementedException();
+            if (numbers == null)
+            {
+                return false;
+            }
+            return numbers.Sum() % 2 != 0;
         }
 
-        public long CountOfPositiveOddsBelowNumber(long number)
+        public long CountOfPositiveOddsBelowNumber(long number) //This mean that the number will be huge.
         {
-            throw new NotImplementedException();
+            if (number <= 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return number / 2;
+            }
         }
     }
 }
