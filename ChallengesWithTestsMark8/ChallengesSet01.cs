2d032bbe89ecc3de2cbbc1dad117ed94455a124c
Dlove <dlove327@gmail.com>
2025-05-13 20:52:53 -0500

    Weekly3

--- ChallengesWithTestsMark8/ChallengesSet01.cs
+++ ChallengesWithTestsMark8/ChallengesSet01.cs
@@ -6,7 +6,7 @@ namespace ChallengesWithTestsMark8
     {
         public bool AreTwoNumbersTheSame(int num1, int num2)
         {
-            if(num1 == num2)
+            if(num1 == num2) //Run a normal if scope asking if the variables are equal.
             {
                 return true;
             }
@@ -14,19 +14,24 @@ namespace ChallengesWithTestsMark8
             {
                 return false;
             }
+            //To simplify: return num1 == num2; This is because it also returns a value.
         }
 
         public double Subtract(double minuend, double subtrahend)
         {
-            return minuend - subtrahend;
+            var answer = minuend - subtrahend;
+            return answer;
+            //return minuend - subtrahend; This is a simplified answer.
         }
 
         public int Add(int number1, int number2)
         {
-            return number1 + number2;
+            var answer = number1 + number2;
+            return answer;
+            //return number1 + number2;
         }
 
-        public int GetSmallestNumber(int number1, int number2)
+        public int GetSmallestNumber(int number1, int number2) 
         {
             if (number1 < number2)
             {
@@ -36,28 +41,31 @@ namespace ChallengesWithTestsMark8
             {
                 return number2;
             }
+            //return number1 < number2 ? number1 : number2; This would be the ternary option. 
         }
 
-        public long Multiply(long factor1, long factor2)
+        public long Multiply(long factor1, long factor2) //This is straight forward. We get two nums and mult.
         {
-            return factor1 * factor2;
+            return factor1 * factor2; //We just simply mult the nums.
         }
 
-        public string GetGreeting(string nameOfPerson)
+        public string GetGreeting(string nameOfPerson) //If this string is empty, then return "Hello".
         {
-            if (nameOfPerson == "")
-            {
-                return "Hello!";
-            }
-            else
-            {
-                return $"Hello, {nameOfPerson}!";
-            }
+            // if (nameOfPerson == "")
+            // {
+            //     return "Hello!";
+            // }
+            // else
+            // {
+            //     return $"Hello, {nameOfPerson}!";
+            // }
+            return string.IsNullOrEmpty(nameOfPerson) ? "Hello!" : $"Hello, {nameOfPerson}!"; //If IsNullOrEmpty comes backs as true, it'll return "Hello!" else it'll return the interpolated statement.
         }
 
-        public string GetHey()
-        {
-            return "HEY!";
-        }
+        // public string GetHey() //This method is a fact meaning it doesn't take any parameters. A theory is a method that does take parameters. 
+        // {
+        //     return "HEY!"; //On the test tab, I'm going to put the string inside "actual" and then "assert" that that actual value is equal to "HEY!"
+        // } 
+            public string GetHey() => "HEY!"; //I could also get fancy and use Lambda instead.
     }
 }
--- ChallengesWithTestsMark8/obj/Debug/net6.0/ChallengesWithTestsMark8.AssemblyInfo.cs
+++ ChallengesWithTestsMark8/obj/Debug/net6.0/ChallengesWithTestsMark8.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ChallengesWithTestsMark8")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7aaef931720833993522d7d5834764c067c75677")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+435a8f3030a2243b84277e6d6505d0e224540821")]
 [assembly: System.Reflection.AssemblyProductAttribute("ChallengesWithTestsMark8")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ChallengesWithTestsMark8")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
